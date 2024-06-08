3af6ebf7b6f487cfd722a621b215f80b09cf9ec0
SylwekD <rutki2268@gmail.com>
2024-06-08 17:59:37 +0200

    AddSwagger

--- Restaurants.API/Controllers/RestaurantsController.cs
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -16,14 +16,14 @@ namespace Restaurants.API.Controllers;
 public class RestaurantsController(IMediator mediator) : ControllerBase
 {
     [HttpGet]
-    public async Task<IActionResult> GetAll()
+    public async Task<ActionResult<IEnumerable<RestaurantDto>>> GetAll()
     {
         var restaurants = await mediator.Send(new GetAllRestaurantsQuery());
         return Ok(restaurants);
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetOneByID([FromRoute]int id)
+    public async Task<ActionResult<RestaurantDto?>> GetOneByID([FromRoute]int id)
     {
         var restaurant = await mediator.Send(new GetOneByIdQuery(id));
 
@@ -34,6 +34,8 @@ public class RestaurantsController(IMediator mediator) : ControllerBase
     }
 
     [HttpPatch("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> UpdateRestaurant([FromRoute] int id, UpdateRestaurantCommand command)
     {
         command.Id = id;
@@ -46,6 +48,8 @@ public class RestaurantsController(IMediator mediator) : ControllerBase
     }
 
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> DeleteRestaurant([FromRoute] int id)
     {
         var isDeleted = await mediator.Send(new DeleteRestaurantCommand(id));
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddApplication();
@@ -27,6 +28,12 @@ await seeder.Seed();
 // Configure the HTTP request pipeline.
 
 app.UseSerilogRequestLogging();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
