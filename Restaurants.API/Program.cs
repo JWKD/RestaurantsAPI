778d1f0260345a44a6ac2d2e076139f2d8552477
SylwekD <rutki2268@gmail.com>
2024-06-10 23:18:24 +0200

    ExtendedUserDefinition

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -39,7 +39,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGroup("api/identity").MapIdentityApi<User>();
+app.MapGroup("api/identity").WithTags("Identity").MapIdentityApi<User>();
 
 app.UseAuthorization();
 
